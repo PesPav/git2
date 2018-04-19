19c6721ed07d5f482cdcfd4536a61c2dd29c8f26
pestryakov123@mail.ru <pestryakov123@mail.ru>
2018-04-19 11:00:51 +0300

    mulriplication added

--- main.c
+++ main.c
@@ -8,6 +8,6 @@ int main()
 	int a,b;
 	printf("Enter two integers:\n");
 	scanf("%d\n%d", &a, &b);
-	printf("sum= %d\n dif= %d\n", a + b, a - b);
+	printf("sum= %d\n mult= %d\n", a + b, a * b);
 	return 0;
 }
